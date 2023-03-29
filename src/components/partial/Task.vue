bd3f655577c08bf0cf9c7d35dcdaf911f7387abe
SamanSah <sahraei.saman91@gmail.com>
2023-03-30 02:26:12 +0430

    update task

--- src/components/partial/Task.vue
+++ src/components/partial/Task.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="tasks-section" ref="tasks_section">
         <template v-for="(task, index) in taskItems" :key="index">
-            <div :style="{backgroundColor: task.background_color}" class="task-item" >
+            <div :style="{backgroundColor: task.task_background_color}" class="task-item" >
                 <nav class="task-nav">
-                    <h5 :style="{color: task.title_color}">{{ task.title }}</h5>
+                    <h5 :style="{color: task.task_title_color}">{{ task.task_title }}</h5>
                     <div>
-                        <fa icon="fa-solid fa-edit" class="me-3" />
+                        <fa @click="$emit('showModalUpdateTask', {index, task})" icon="fa-solid fa-edit" class="me-3" />
                         <fa @click="this.$parent.deleteTask(index)" icon="fa-solid fa-trash" />
                     </div>
                 </nav>
-                <template v-if="task.short_description">
+                <template v-if="task.task_desciption">
                     <hr>
-                    <div :style="{color: task.description_color}" class="task-body">
-                        {{ task.short_description }}
+                    <div :style="{color: task.task_description_color}" class="task-body">
+                        {{ task.task_desciption }}
                     </div>
                 </template>  
                 <!-- <div class="task-date-section">
