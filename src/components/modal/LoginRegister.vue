1c3519dd35eff308afd25e88f3b79d4850ac05ad
SamanSah <sahraei.saman91@gmail.com>
2023-04-16 00:03:43 +0430

    fix placeholder inputs

--- src/components/modal/LoginRegister.vue
+++ src/components/modal/LoginRegister.vue
@@ -7,9 +7,9 @@
             <div v-if="login_register_form" class="register-form">
                 <form  class="register-form mb-lg-4" @submit.prevent="this.$emit('register', register_form)">
                     <h2>Register</h2>
-                    <input type="text" name="name" id="name" v-model="register_form.name">
-                    <input type="email" placeholder="Enter Email" name="" v-model="register_form.email">
-                    <input type="password" placeholder="Enter password" name="" v-model="register_form.password">
+                    <input type="text" name="name" id="name" placeholder="Name" v-model="register_form.name">
+                    <input type="email" placeholder="Email" name="" v-model="register_form.email">
+                    <input type="password" placeholder="Password" name="" v-model="register_form.password">
                     <input class="btn btn-primary" type="submit" value="Rgistre">
                 </form>
                 <div class="login-register-link">
@@ -19,8 +19,8 @@
             <div v-else class="login-form">
                 <form  class="login-form mb-lg-4" @submit.prevent="this.$emit('login', login_form)">
                     <h2>Login</h2>
-                    <input type="email" placeholder="Enter Email" name="" v-model="login_form.email">
-                    <input type="password" placeholder="Enter password" name="" v-model="login_form.password">
+                    <input type="email" placeholder="Email" name="" v-model="login_form.email">
+                    <input type="password" placeholder="Password" name="" v-model="login_form.password">
                     <input type="submit" class="btn btn-primary" value="Login">
                 </form>
                 <div class="login-register-link">
